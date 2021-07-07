dbff63a0d0a525657d797c15396b12bf43c84ba4
Naval Bihani <bihani.naval13gmail.com>
2021-07-07 16:31:35 +0530

    Remove unnecessary Console.logs Calls

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,16 @@
-console.log("Client Side Javascript is loaded!!!");
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
-const weatherForm = document.querySelector("form");
-const search = document.querySelector("input");
-const messageOne = document.querySelector("#message-1");
-const messageTwo = document.querySelector("#message-2");
-
-weatherForm.addEventListener("submit", (e) => {
+weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
   //   console.log("testing!!!");
 
   const location = search.value;
 
-  messageOne.textContent = "Loading....";
-  messageTwo.textContent = " ";
+  messageOne.textContent = 'Loading....';
+  messageTwo.textContent = ' ';
 
   fetch(`http://localhost:3000/weather?address=${location}`)
     .then((response) => response.json())
