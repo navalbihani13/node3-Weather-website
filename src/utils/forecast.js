be7194f6669fa54adc84e74ee10c344968e4e6fa
Naval Bihani <bihani.naval13gmail.com>
2021-07-08 15:20:57 +0530

    Adding Some More Information to the weather app

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,17 +1,17 @@
-const request = require("postman-request");
+const request = require('postman-request');
 
 const forecast = (lat, long, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=c367e2c136052a54fa7b03666337d666&query=${lat},${long}`;
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
-      callback("Unable to connect to Weather Service!!!", undefined);
+      callback('Unable to connect to Weather Service!!!', undefined);
     } else if (body.error) {
-      callback("Unable to find Location!!!", undefined);
+      callback('Unable to find Location!!!', undefined);
     } else {
       callback(
         undefined,
-        `It is ${body.current.weather_descriptions[0]} today .It is ${body.current.temperature} degress today. It feels like ${body.current.feelslike} degress out!!!`
+        `It is ${body.current.weather_descriptions[0]} today .It is ${body.current.temperature} degress today. It feels like ${body.current.feelslike} degress out!!!. Humidity is ${body.current.humidity}`
       );
     }
   });
